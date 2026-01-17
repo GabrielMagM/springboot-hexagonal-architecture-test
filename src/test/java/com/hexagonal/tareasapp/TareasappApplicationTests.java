0336671ab6db05d65a1362f99742c34bc92e9722
WaxXaa <mfalejesus@criptext.com>
2026-01-16 20:31:09 -0500

    first commit

---
+++ src/test/java/com/hexagonal/tareasapp/TareasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.tareasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
